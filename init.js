b02429361c09225e6bb79b9164168bc28df266de
Szymon Wygnanski <szymon@spotme.com>
2015-10-03 21:00:50 +0200

    -

--- init.js
+++ init.js
@@ -1,13 +1,25 @@
 'use babel';
 
-import {} from './custom/atom-commander-enhancements';
-import {} from './custom/cursor-enhancements';
-
+import {} from './lib/atom-commander-enhancements';
+import {} from './lib/cursor-enhancements';
+import {PackagesInstaller} from './lib/PackagesInstaller';
 import {exec} from 'child_process';
+
+var bootstrapper = atom.packages.getActivePackage('bootstrapper').mainModule;
+bootstrapper.bootstrap('./lib/boot');
+
 
 exec("$SHELL --login -i -c 'echo $PATH'", function (error, stdout, stderr) {
     process.env.PATH = stdout;
     if (error !== null) {
         console.log('exec error: ' + error);
     }
+
+    var pkgInstaller = new PackagesInstaller();
+
+    pkgInstaller.add
 });
+
+pkgInstaller.install([
+
+]);
